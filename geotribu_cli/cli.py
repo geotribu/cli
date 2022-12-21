2a67b114eb98d95c78d02e26c5d469f35406d427
GeoJulien <dev@ingeoveritas.com>
2022-12-21 16:19:01 +0100

    Add subcommand search-image

---
+++ geotribu_cli/cli.py
@@ -0,0 +1,113 @@
+#! python3  # noqa: E265
+
+"""Main CLI entrypoint."""
+
+# standard lib
+import argparse
+import logging
+import sys
+from typing import List
+
+# package
+from geotribu_cli.__about__ import (
+    __author__,
+    __cli_usage__,
+    __summary__,
+    __title__,
+    __title_clean__,
+    __uri_homepage__,
+    __version__,
+)
+from geotribu_cli.subcommands.search_image import parser_search_image
+
+# ############################################################################
+# ########## MAIN ################
+# ################################
+
+# this serves as a parent parser
+def add_common_arguments(parser_to_update):
+    parser_to_update.add_argument(
+        "-v",
+        "--verbose",
+        action="count",
+        default=1,
+        dest="verbosity",
+        help="Niveau de verbosité : None = WARNING, -v = INFO, -vv = DEBUG",
+    )
+    return parser_to_update
+
+
+def main(argv: List[str] = None):
+    """Main CLI entrypoint."""
+    # create the top-level parser
+    main_parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        epilog=f"{__cli_usage__}\n\n"
+        f"Développé par {__author__}\n"
+        f"Documentation : {__uri_homepage__}",
+        description=f"{__title__} {__version__} - {__summary__}",
+    )
+
+    # -- ROOT ARGUMENTS --
+
+    # Optional verbosity counter (eg. -v, -vv, -vvv, etc.)
+    main_parser.add_argument(
+        "-v",
+        "--verbose",
+        action="count",
+        default=1,
+        dest="verbosity",
+        help="Niveau de verbosité : None = WARNING, -v = INFO, -vv = DEBUG",
+    )
+
+    main_parser.add_argument(
+        "--version",
+        action="version",
+        version=__version__,
+    )
+
+    # -- PARSE PASSED ARGUMENTS --
+    subparsers = main_parser.add_subparsers(title="Sous-commandes", dest="command")
+
+    subcmd_search_image = subparsers.add_parser(
+        "search-image", help="Rechercher dans les images du CDN"
+    )
+    add_common_arguments(subcmd_search_image)
+    parser_search_image(subcmd_search_image)
+
+    # get passed args and force print help if none
+    # args = main_parser.parse_args(None if sys.argv[1:] else ["-h"])
+
+    # just get passed args
+    args = main_parser.parse_args(argv)
+
+    # set log level depending on verbosity argument
+    if 0 < args.verbosity < 4:
+        args.verbosity = 40 - (10 * args.verbosity)
+    elif args.verbosity >= 4:
+        # debug is the limit
+        args.verbosity = 40 - (10 * 3)
+    else:
+        args.verbosity = 0
+
+    logging.basicConfig(
+        level=args.verbosity,
+        format="%(asctime)s||%(levelname)s||%(module)s||%(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+
+    console = logging.StreamHandler()
+    console.setLevel(args.verbosity)
+
+    # add the handler to the root logger
+    logger = logging.getLogger(__title_clean__)
+    logger.debug(f"Log level set: {logging.getLevelName(args.verbosity)}")
+
+    # -- RUN LOGIC --
+    logger.info("piou")
+    args.func(args)
+
+
+# -- Stand alone execution
+if __name__ == "__main__":
+    main()  # required by unittest
---
+++ geotribu_cli/constants.py
@@ -0,0 +1,39 @@
+#! python3  # noqa: E265
+
+
+"""Package constants."""
+
+# standard library
+from dataclasses import dataclass
+
+
+@dataclass
+class GeotribuDefaults:
+    """Defaults settings for Geotribu."""
+
+    # git
+    git_base_url_organisation: str = "https://github.com/geotribu/"
+    # website
+    site_base_url: str = "https://static.geotribu.fr/"
+    site_git_project: str = "website"
+    # cdn
+    cdn_base_url: str = "https://cdn.geotribu.fr/"
+    cdn_base_path: str = "img"
+    cdn_search_index: str = "search-index.json"
+    # comments
+    comments_base_url: str = "https://comments.geotribu.fr/"
+
+    @property
+    def cdn_search_index_full_url(self) -> str:
+        """Returns CDN search index full URL.
+
+        Returns:
+            str: URL as string
+        """
+        return f"{self.cdn_base_url}{self.cdn_base_path}/{self.cdn_search_index}"
+
+
+# -- Stand alone execution
+if __name__ == "__main__":
+    defaults = GeotribuDefaults()
+    print(defaults.cdn_search_index_full_url)
