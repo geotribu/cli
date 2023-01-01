d3f7d0855258a7ecbf89cfa940c75f48900ca997
GeoJulien <dev@ingeoveritas.com>
2023-01-01 17:37:03 +0100

    Add PyInstaller script for MacOS

---
+++ builder/pyinstaller_build_macos.py
@@ -0,0 +1,46 @@
+#! python3  # noqa: E265
+
+"""
+    Launch PyInstaller using a Python script.
+"""
+
+# #############################################################################
+# ########## Libraries #############
+# ##################################
+
+# Standard library
+import platform
+import sys
+from os import getenv
+from pathlib import Path
+
+# 3rd party
+import PyInstaller.__main__
+
+# package
+sys.path.insert(0, str(Path(".").resolve()))
+from geotribu_cli import __about__  # noqa: E402
+
+# #############################################################################
+# ########### MAIN #################
+# ##################################
+package_folder = Path("geotribu_cli")
+
+PyInstaller.__main__.run(
+    [
+        "--log-level={}".format(getenv("PYINSTALLER_LOG_LEVEL", "WARN")),
+        "--name={}_{}_{}{}_Python{}-{}".format(
+            __about__.__title_clean__,
+            __about__.__version__.replace(".", "-"),
+            platform.system(),
+            platform.architecture()[0],
+            platform.python_version_tuple()[0],
+            platform.python_version_tuple()[1],
+        ),
+        "--noconfirm",
+        "--noupx",
+        "--onefile",
+        "--console",
+        str(package_folder / "cli.py"),
+    ]
+)
