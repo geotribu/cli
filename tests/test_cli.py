66c729f9b9221f664c569152d94c84a2e6a5b5d4
GeoJulien <dev@ingeoveritas.com>
2023-01-14 00:01:20 +0100

    Fix cli tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -20,47 +20,62 @@ from geotribu_cli import __about__, cli
 # ################################
 
 
-def test_cli_run_rss(capsys):
-    """Test subcommand rss."""
+@pytest.mark.parametrize("option", ("-h", "--help"))
+def test_cli_help(capsys, option):
+    """Test CLI help."""
     with pytest.raises(SystemExit):
-        cli.main(["rss", "-o", "table"])
+        cli.main([option])
 
-        out, err = capsys.readouterr()
+    out, err = capsys.readouterr()
 
-        assert out == ""
-        assert err == ""
+    assert (
+        f"{__about__.__title__} {__about__.__version__} - {__about__.__summary__}"
+        in out
+    )
+    assert err == ""
 
 
-def test_cli_run_images(capsys):
-    """Test subcommand images."""
+@pytest.mark.parametrize("option", (["--version"]))
+def test_cli_version(capsys, option):
+    """Test CLI version."""
     with pytest.raises(SystemExit):
-        cli.main(["images", "-f", "logo", "-o", "table", "news"])
+        cli.main([option])
 
-        out, err = capsys.readouterr()
+    out, err = capsys.readouterr()
 
-        assert out == ""
-        assert err == ""
+    assert f"{__about__.__version__}\n" == out
+
+    assert err == ""
+
+
+def test_cli_run_contenus_articles_ubuntu(capsys):
+    """Test CLI images."""
+    cli.main(["search-content", "-f", "article", "-o", "table", "title:ubuntu"])
+
+    out, err = capsys.readouterr()
+
+    # assert out.startswith("[")
+    assert err == ""
+
+    assert Path(Path().home() / ".geotribu/search/site_search_index.json").exists()
+
+
+def test_cli_run_images_logo_news(capsys):
+    """Test CLI images."""
+    cli.main(["images", "-f", "logo", "news"])
+
+    out, err = capsys.readouterr()
+
+    assert out.startswith("[")
+    assert err == ""
 
     assert Path(Path().home() / ".geotribu/search/cdn_search_index.json").exists()
 
 
-def test_cli_help(capsys):
-    """Test CLI help."""
-    with pytest.raises(SystemExit):
-        cli.main(["--help"])
+def test_cli_run_rss(capsys):
+    """Test subcommand rss."""
+    cli.main(["rss", "-o", "table"])
 
-        out, err = capsys.readouterr()
+    out, err = capsys.readouterr()
 
-        assert __about__.__cli_usage__ in out
-        assert err == ""
-
-
-def test_cli_version(capsys):
-    """Test CLI version."""
-    with pytest.raises(SystemExit):
-        cli.main(["--version"])
-
-        out, err = capsys.readouterr()
-
-        assert out[0] == f"{__about__.__version__}\n"
-        assert err == ""
+    assert err == ""
