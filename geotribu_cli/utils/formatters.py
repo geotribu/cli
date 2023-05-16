2562840d7dbf7c5645a3b18ca7f752efd5b5aed9
GeoJulien <dev@ingeoveritas.com>
2023-05-16 17:35:27 +0200

    Add utils to manipulate URLs query params

--- geotribu_cli/utils/formatters.py
+++ geotribu_cli/utils/formatters.py
@@ -2,9 +2,15 @@
 
 """Helpers to format text and variables."""
 
+
 # standard library
+from functools import lru_cache
 from math import floor
 from math import log as math_log
+from urllib.parse import parse_qs, urlsplit, urlunsplit
+
+# package
+from geotribu_cli.__about__ import __title_clean__, __version__
 
 # ############################################################################
 # ########## FUNCTIONS ###########
@@ -40,3 +46,39 @@ def convert_octets(octets: int) -> str:
     s = round(octets / p, 2)
 
     return f"{s} {size_name[i]}"
+
+
+@lru_cache(maxsize=256, typed=True)
+def url_rm_query(in_url: str, param_startswith: str = "utm_") -> str:
+    """Remove existing utm_* query parameters from input URL.
+
+    Returns:
+        str: URL without utm_* query parameters
+    """
+    parsed_url = urlsplit(url=in_url)
+    url_query_in = parse_qs(parsed_url.query)
+    url_query_out = {}
+    for param, value in url_query_in.items():
+        if param.startswith(param_startswith):
+            continue
+        url_query_out[param] = value
+
+    return urlunsplit(parsed_url._replace(query=url_query_out))
+
+
+@lru_cache(maxsize=256, typed=True)
+def url_add_utm(in_url: str) -> str:
+    """Adds utm_* query parameters to the item URL.
+
+    Returns:
+        str: URLs with utm_* query parameters to track openings from this package.
+    """
+    parsed_url = urlsplit(url=url_rm_query(in_url))
+
+    return urlunsplit(
+        parsed_url._replace(
+            query=f"utm_source=geotribu_cli"
+            f"&utm_medium={__title_clean__}"
+            f"&utm_campaign=geotribu_cli_{__version__}"
+        )
+    )
--- tests/test_utils_formatters.py
+++ tests/test_utils_formatters.py
@@ -14,7 +14,7 @@
 import unittest
 
 # project
-from geotribu_cli.utils.formatters import convert_octets
+from geotribu_cli.utils.formatters import convert_octets, url_add_utm, url_rm_query
 
 # ############################################################################
 # ########## Classes #############
@@ -34,6 +34,22 @@ class TestUtilsFormatters(unittest.TestCase):
         self.assertEqual(
             convert_octets(2097152),
             "2.0 Mo",
+        )
+
+    def test_url_rm_query_parameters(self):
+        """Test URL query params manipulations."""
+        self.assertEqual(
+            url_rm_query(
+                in_url="https://geotribu.fr/rdp/2023/rdp_2023-05-12/?utm_campaign=feed-syndication&utm_medium=RSS&utm_source=rss-feed",
+                param_startswith="utm_",
+            ),
+            "https://geotribu.fr/rdp/2023/rdp_2023-05-12/",
+        )
+        self.assertEqual(
+            url_add_utm(
+                in_url="https://geotribu.fr/rdp/2023/rdp_2023-05-12/?utm_campaign=feed-syndication&utm_medium=RSS&utm_source=rss-feed",
+            ),
+            "https://geotribu.fr/rdp/2023/rdp_2023-05-12/?utm_source=geotribu_cli&utm_medium=GeotribuToolbelt&utm_campaign=geotribu_cli_0.14.0",
         )
 
 
