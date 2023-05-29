483b4c100cd996da5074688468370f970b0019be
GeoJulien <dev@ingeoveritas.com>
2023-05-29 23:51:51 +0200

    Add subcommand to get latest comments

--- geotribu_cli/subcommands/__init__.py
+++ geotribu_cli/subcommands/__init__.py
@@ -1,7 +1,7 @@
 #! python3  # noqa: E265
 
 # submodules
-
+from .comments_latest import parser_comments_latest  # noqa: F401
 from .open_result import parser_open_result  # noqa: F401
 from .rss_reader import parser_latest_content  # noqa: F401
 from .search_content import parser_search_content  # noqa: F401
